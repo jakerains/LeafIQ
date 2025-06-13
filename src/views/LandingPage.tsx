1e03eea8a18fe105a407be266814a62214510498
GenAI Jake <jakerains@gmail.com>
2025-06-13 16:59:41 -0500

    Add Spacing Between Hero Content and Image

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -229,7 +229,7 @@ const LandingPage = () => {
               </motion.div>
             </div>
             <div className="flex-1 order-2 hidden lg:block">
-              <div className="flex justify-center">
+              <div className="flex justify-center pl-6"> {/* Added left padding (pl-6) to create space */}
                 <motion.div
                   initial={{ opacity: 0, scale: 0.9, y: 20 }}
                   animate={{ opacity: 1, scale: 1, y: 0 }}
